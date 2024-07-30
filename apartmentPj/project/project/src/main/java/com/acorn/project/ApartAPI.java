a05d0bff87412a73c6b08b4ee962eaa5da8fbce4
yewon_kim <wonwhimsical@gmail.com>
2024-07-30 15:32:47 +0900

    chage api

--- apartmentPj/project/project/src/main/java/com/acorn/project/ApartAPI.java
+++ apartmentPj/project/project/src/main/java/com/acorn/project/ApartAPI.java
@@ -10,12 +10,15 @@ import java.io.IOException;
 
 public class ApartAPI {
     public static void main(String[] args) throws IOException {
-        StringBuilder urlBuilder = new StringBuilder("http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev"); /*URL*/
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/FmlandWthrInfoService/getDayStatistics"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=XHhofBD0jO01cbj7mByrLZmfjk4RCQk%2Fv5Rvx7oXy%2FdhxB0PpL5LIVKGrw3jMiYMphG8vIw8BJTsL876ezO44A%3D%3D"); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("LAWD_CD","UTF-8") + "=" + URLEncoder.encode("11110", "UTF-8")); /*지역코드*/
-        urlBuilder.append("&" + URLEncoder.encode("DEAL_YMD","UTF-8") + "=" + URLEncoder.encode("201512", "UTF-8")); /*계약월*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("XML", "UTF-8")); /*요청자료형식(XML/JSON)*/
+        urlBuilder.append("&" + URLEncoder.encode("ST_YMD","UTF-8") + "=" + URLEncoder.encode("20161201", "UTF-8")); /*일통계 시작 날짜(YYYYMMDD)*/
+        urlBuilder.append("&" + URLEncoder.encode("ED_YMD","UTF-8") + "=" + URLEncoder.encode("20161201", "UTF-8")); /*일통계 종료 날짜(YYYYMMDD)*/
+        urlBuilder.append("&" + URLEncoder.encode("AREA_ID","UTF-8") + "=" + URLEncoder.encode("4122000000", "UTF-8")); /*지역 아이디(활용가이드 하단첨부 참고)*/
+        urlBuilder.append("&" + URLEncoder.encode("PA_CROP_SPE_ID","UTF-8") + "=" + URLEncoder.encode("PA130201", "UTF-8")); /*주산지 작물별 특성 아이디(활용가이드 하단첨부 참고)*/
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
