57283edc31b991bfd4536b1252a494ad1e5c0247
yewon_kim <wonwhimsical@gmail.com>
2024-07-31 13:00:31 +0900

    좌표계 이상

--- front/kakaomap/jsondata.js
+++ front/kakaomap/jsondata.js
@@ -11,12 +11,28 @@ fs.readFile('korea.json', 'utf-8', (err, data) => {
     }
     const jsonData = JSON.parse(data);
     const features = jsonData.features;
-//     features.forEach(sido => {
-//         console.log(sido);
-//    });
 
     const sido = features.find(feature => feature.properties.CTP_ENG_NM == "Busan");
     const coordinates = sido.geometry.coordinates;
     console.log(coordinates);
+
+    coordinates.forEach(innerArray => {
+        innerArray.forEach(coord => {
+            coord.forEach(value => {
+                console.log(`X: ${value[0]}, Y: ${value[1]}`, ':',count);
+            })
+        });
+    });
+    
+
+    // for (let i = 0; i < coordinates.length; i++) { // 첫 번째 차원: 지역
+    //     for (let j = 0; j < coordinates[i].length; j++) { // 두 번째 차원: 폴리곤
+    //       for (let k = 0; k < coordinates[i][j].length; k++) { // 세 번째 차원: 좌표 점
+    //         const point = coordinates[i][j][k]; // [X, Y] 배열
+    //         console.log(`지역 ${i + 1}, 폴리곤 ${j + 1}, 좌표 점 ${k + 1} - X: ${point[0]}, Y: ${point[1]}`);
+    //       }
+    //     }
+    //   }
+    
 });
 
