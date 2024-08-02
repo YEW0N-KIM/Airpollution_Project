e300030d0e05913bdf98f88602a3df64a807a2d8
yewon_kim <wonwhimsical@gmail.com>
2024-08-03 02:29:56 +0900

    api 연결 1차 정리

--- apartmentPj/project/project/src/main/java/com/acorn/project/ApiController.java
+++ apartmentPj/project/project/src/main/java/com/acorn/project/ApiController.java
@@ -13,20 +13,22 @@ import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class ApiController {
-    
+	
 	@GetMapping("/api")
-	public String averageAir(Model model) throws IOException {
+	public String airReg(@RequestParam (value ="sido", defaultValue = "서울", required = false )String sido, Model model) throws IOException {
+		
 		//	시도별 실시간 측정정보 조회
 	 	StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty");
 	 	urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=XHhofBD0jO01cbj7mByrLZmfjk4RCQk%2Fv5Rvx7oXy%2FdhxB0PpL5LIVKGrw3jMiYMphG8vIw8BJTsL876ezO44A%3D%3D");
         urlBuilder.append("&" + URLEncoder.encode("returnType","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
         urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("50", "UTF-8")); /*한 페이지 결과 수(조회 날짜로 검색 시 사용 안함)*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호(조회 날짜로 검색 시 사용 안함)*/
-        urlBuilder.append("&" + URLEncoder.encode("sidoName","UTF-8") + "=" + URLEncoder.encode("부산", "UTF-8")); /*시도*/
+        urlBuilder.append("&" + URLEncoder.encode("sidoName","UTF-8") + "=" + URLEncoder.encode(sido, "UTF-8")); /*시도*/
         urlBuilder.append("&" + URLEncoder.encode("ver","UTF-8") + "=" + URLEncoder.encode("1.4", "UTF-8")); /* 버전 */
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
